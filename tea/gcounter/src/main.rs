a9e83c4343b207b94b5b7524c6556664b9ab3bee
Arumugam Ramaswamy <rm.arumugam.2000@gmail.com>
2026-03-28 23:09:57 +0800

    update gcounter to work

--- tea/gcounter/src/main.rs
+++ tea/gcounter/src/main.rs
@@ -1,141 +1,172 @@
+use std::collections::HashMap;
+use std::time::Duration;
+
 use serde::{Deserialize, Serialize};
 use serde_json;
 use tokio::io::{self, AsyncBufReadExt, BufReader, Lines, Stdin};
 use tokio::sync::mpsc;
+use tokio::time::interval;
 
 #[tokio::main]
 async fn main() {
-    let (comms_handler, mut comms_client) = create_comms();
-    tokio::spawn(comms_handler.communication_loop());
+    let mut receiver = Receiver::new();
+    let my_id = perform_init(&mut receiver).await;
+    let mut state = GCounterState::new(my_id);
 
-    let mut my_msg_id = 0;
-    let my_id;
-    let mut next_gen_id;
-    let n_nodes;
-    let msg = comms_client.msg_channel.recv().await.unwrap();
+    let mut interval = interval(Duration::from_millis(200));
 
+    loop {
+        tokio::select! {
+            msg = receiver.recv() => {
+                state.handle_message(msg).await
+            },
+            _ = interval.tick() => {
+                state.trigger_reconciliation().await
+            }
+        }
+    }
+}
+
+struct GCounterState {
+    remaining: u32,
+    current_state: u32,
+    node_name: String,
+    request_id_to_delta: HashMap<u32, u32>,
+    message_id: u32,
+}
+
+impl GCounterState {
+    fn new(my_id: String) -> Self {
+        Self {
+            remaining: 0,
+            current_state: 0,
+            node_name: my_id,
+            request_id_to_delta: HashMap::new(),
+            message_id: 0,
+        }
+    }
+
+    async fn trigger_reconciliation(&mut self) {
+        let msg_id = {
+            self.message_id += 1;
+            self.message_id
+        };
+
+        self.request_id_to_delta.insert(msg_id, self.remaining);
+
+        send(
+            "seq-kv".to_string(),
+            self.node_name.clone(),
+            MessageBody::Cas {
+                msg_id,
+                key: "k".to_string(),
+                from: self.current_state,
+                to: self.current_state + self.remaining,
+                create_if_not_exists: true,
+            },
+        )
+        .await;
+    }
+
+    async fn handle_message(&mut self, msg: Message) {
+        match msg.body {
+            MessageBody::Read { msg_id, .. } => {
+                send(
+                    msg.src,
+                    self.node_name.clone(),
+                    MessageBody::ReadOk {
+                        in_reply_to: msg_id,
+                        value: self.current_state,
+                    },
+                )
+                .await
+            }
+            MessageBody::ReadOk { value, .. } => {
+                if value > self.current_state {
+                    self.current_state = value;
+                }
+            }
+            MessageBody::Add { delta, msg_id } => {
+                self.remaining += delta;
+                send(
+                    msg.src,
+                    self.node_name.clone(),
+                    MessageBody::AddOk {
+                        in_reply_to: msg_id,
+                    },
+                )
+                .await;
+            }
+            MessageBody::CasOk { in_reply_to } => {
+                self.remaining -= self.request_id_to_delta.remove(&in_reply_to).unwrap();
+            }
+            MessageBody::Error { in_reply_to } => {
+                let msg_id = {
+                    self.message_id += 1;
+                    self.message_id
+                };
+                self.request_id_to_delta.remove(&in_reply_to).unwrap();
+                send(
+                    "seq-kv".to_string(),
+                    self.node_name.clone(),
+                    MessageBody::Read {
+                        msg_id,
+                        key: Some("k".to_string()),
+                    },
+                )
+                .await
+            }
+            _ => unreachable!(),
+        }
+    }
+}
+
+async fn perform_init(receiver: &mut Receiver) -> String {
+    let msg = receiver.recv().await;
     match msg.body {
         MessageBody::Init {
-            msg_id,
-            node_id,
-            node_ids,
+            msg_id, node_id, ..
         } => {
-            my_id = node_id;
-            n_nodes = node_ids.len();
-            next_gen_id = node_ids.iter().position(|s| *s == my_id).unwrap() + 1;
-
-            let resp = Message {
-                dest: msg.src,
-                src: my_id.clone(),
-                body: MessageBody::InitOk {
+            let _ = send(
+                msg.src,
+                node_id.clone(),
+                MessageBody::InitOk {
                     in_reply_to: msg_id,
                 },
-            };
-            let _ = comms_client.response_channel.send(resp).await;
+            )
+            .await;
+            node_id
         }
         _ => panic!("init message expected"),
     }
+}
 
-    // Read each line asynchronously
-    while let Some(msg) = comms_client.msg_channel.recv().await {
-        match msg.body {
-            MessageBody::Read { msg_id, .. } => {
-                let resp = Message {
-                    dest: msg.src,
-                    src: my_id.clone(),
-                    body: MessageBody::ReadOk {
-                        in_reply_to: msg_id,
-                        value: 0,
-                    },
-                };
-                let _ = comms_client.response_channel.send(resp).await;
-            }
-            MessageBody::Add { msg_id, delta } => {
-                let cas = Message {
-                    dest: "seq-kv".to_string(),
-                    src: my_id.clone(),
-                    body: MessageBody::Cas {
-                        msg_id: {
-                            my_msg_id += 1;
-                            my_msg_id
-                        },
-                        key: "k1".to_string(),
-                        from: 0,
-                        to: delta,
-                        create_if_not_exists: true,
-                    },
-                };
-                // let resp = Message {
-                //     dest: msg.src,
-                //     src: my_id.clone(),
-                //     body: MessageBody::AddOk {
-                //         in_reply_to: msg_id,
-                //     },
-                // };
-                let _ = comms_client.response_channel.send(cas).await;
-            }
-            _ => panic!("unexpected"),
-        }
+struct Receiver(tokio::io::Lines<BufReader<Stdin>>);
+
+impl Receiver {
+    fn new() -> Self {
+        Self(BufReader::new(tokio::io::stdin()).lines())
+    }
+
+    async fn recv(&mut self) -> Message {
+        let line = self.0.next_line().await.unwrap().unwrap();
+        eprintln!("Recved Message: {line:?}");
+        let output = serde_json::from_str(&line).unwrap();
+        output
     }
 }
 
-fn create_comms<'a>() -> (CommunicationHandler, CommunicationClient) {
-    let (sender, recver) = mpsc::channel(100);
-    let (sender2, recver2) = mpsc::channel(100);
-
-    let stdin = io::stdin(); // Get the standard input handle
-    let reader = BufReader::new(stdin); // Wrap it in a BufReader for efficiency
-    let lines = reader.lines(); // Create an asynchronous line reader
-    (
-        CommunicationHandler {
-            msg_channel: sender,
-            response_channel: recver2,
-            stdin_lines: lines,
-        },
-        CommunicationClient {
-            msg_channel: recver,
-            response_channel: sender2,
-        },
-    )
-}
-
-struct CommunicationHandler {
-    msg_channel: mpsc::Sender<Message<MessageBody>>,
-    response_channel: mpsc::Receiver<Message<MessageBody>>,
-    stdin_lines: Lines<BufReader<Stdin>>,
-}
-
-impl<'a> CommunicationHandler {
-    async fn communication_loop(mut self) {
-        loop {
-            tokio::select! {
-            Some(resp) = self.response_channel.recv() => {
-                eprintln!("Sending Message: {resp:?}");
-                println!("{}", serde_json::to_string(&resp).unwrap()); // TODO: replace with
-                    // tokio esq way of handling this
-            },
-            Ok(line) = self.stdin_lines.next_line() => {
-                let line = line.unwrap();
-                eprintln!("Recved Message: {line}");
-                let msg: Message<MessageBody> = serde_json::from_str(&line).unwrap();
-                let _ = self.msg_channel.send(msg).await;
-            },
-                else => break,
-            }
-        }
-    }
-}
-
-struct CommunicationClient {
-    msg_channel: mpsc::Receiver<Message<MessageBody>>,
-    response_channel: mpsc::Sender<Message<MessageBody>>,
+async fn send(dest: String, src: String, body: MessageBody) {
+    let msg = Message { dest, src, body };
+    eprintln!("Sending Message: {msg:?}");
+    println!("{}", serde_json::to_string(&msg).unwrap());
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Message<Body> {
+struct Message {
     src: String,
     dest: String,
-    body: Body,
+    body: MessageBody,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -173,6 +204,9 @@ enum MessageBody {
         create_if_not_exists: bool,
     },
     CasOk {
+        in_reply_to: u32,
+    },
+    Error {
         in_reply_to: u32,
     },
 }
